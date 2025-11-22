e0ccfcf318d063c81e36a09aaad3f36afd2d663c
Cristian <cristianlb86@gmail.com>
2025-11-22 19:45:33 +0100

    add database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
+import databaseConfig from "./config/database.config"
+import { DatabaseModule } from "./database/database.module"
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    DatabaseModule
+  ],
   controllers: [],
   providers: [],
 })
